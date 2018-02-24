6b4a0e58db588b62d324aa24cf133d809d90fd7b
mrj0909 <site.testing.1234@gmail.com>
2018-02-24 14:54:10 +0500

    added new function packages to access packages page

--- dashboard/assets/js/core.js
+++ dashboard/assets/js/core.js
@@ -447,6 +447,73 @@ var Actions = {
                 }, 400), window.scrollTo(0, 0), !1
             })
         },
+        packages: function(){
+           $.ajax({
+                type: 'GET',
+                url: 'https://api.subely.com/plans',
+                dataType: 'json',
+                success: function (response) {
+
+                $.each( response, function( key, value ) {
+                   $('#packages-and-plans').append('<div class="col-xs-12 col-md-4">' +
+                                                    '<div class="panel panel-success">' +
+                                                       '<div class="panel-heading">' +
+                                                          '<h3 style="color:black;" class="panel-title">' + value.name +
+                                                            '</h3>' +
+                                                            '</div>' +
+                                                     '<div class="panel-body">' +
+                                                        '<div class="the-price">' +
+                                                          '<h1>' + value.price +
+                                                            '<span class="subscript">/mo</span></h1>' +
+                                                                 '<small>1 month FREE trial</small>' +
+                                                              '</div>' +
+                                                              '<table class="table">' +
+                                                                 '<tr>' +
+                                                                    '<td>' + 
+                                                                        '1 Account' +
+                                                                    '</td>' +
+                                                                  '</tr>' +
+                                                                  '<tr class="active">' +
+                                                                        '<td>' +
+                                                                            '1 Project' +
+                                                                        '</td>' +
+                                                                  '</tr>' +
+                                                                   '<tr>' +
+                                                                    '<td>' +
+                                                                        '100K API Access' +
+                                                                    '</td>' +
+                                                                  '</tr>' +
+                                                                    '<tr class="active">' +
+                                                                      '<td>' +
+                                                                        '100MB Storage' +
+                                                                      '</td>' +
+                                                                  '</tr>' +
+                                                                   '<tr>' +
+                                                                      '<td>' +
+                                                                          'Custom Cloud Services' +
+                                                                      '</td>' +
+                                                                    '</tr>' +
+                                                                  '<tr class="active">' +
+                                                                   '<td>' +
+                                                                      'Weekly Reports' +
+                                                                    '</td>' +
+                                                                    '</tr>' +
+                                                                  '</table>' +
+                                                                  '</div>' +
+                                                                  '<div class="panel-footer">' +
+                                                                    '<a  class="btn btn-success" role="button" data-toggle="modal" data-target="#payment">Sign Up</a>' +
+                                                                    '1 month FREE trial</div>' +
+                                                                    '</div>' +
+                                                                    '</div>' +'');
+                    });
+                },
+                error: function(response) {
+                  console.log('no plans available');
+                }
+
+            });
+
+        },
         forms: function() {
             $(".validate-form").validate({
                 validClass: "valid",
@@ -878,70 +945,7 @@ $(document).ready(function(){
 
 
 if(currentView === 'packages'){
-      $.ajax({
-                type: 'GET',
-                url: 'https://api.subely.com/plans',
-                dataType: 'json',
-                success: function (response) {
-
-                $.each( response, function( key, value ) {
-                   $('#packages-and-plans').append('<div class="col-xs-12 col-md-4">' +
-                                                    '<div class="panel panel-success">' +
-                                                       '<div class="panel-heading">' +
-                                                          '<h3 style="color:black;" class="panel-title">' + value.name +
-                                                            '</h3>' +
-                                                            '</div>' +
-                                                     '<div class="panel-body">' +
-                                                        '<div class="the-price">' +
-                                                          '<h1>' + value.price +
-                                                            '<span class="subscript">/mo</span></h1>' +
-                                                                 '<small>1 month FREE trial</small>' +
-                                                              '</div>' +
-                                                              '<table class="table">' +
-                                                                 '<tr>' +
-                                                                    '<td>' + 
-                                                                        '1 Account' +
-                                                                    '</td>' +
-                                                                  '</tr>' +
-                                                                  '<tr class="active">' +
-                                                                        '<td>' +
-                                                                            '1 Project' +
-                                                                        '</td>' +
-                                                                  '</tr>' +
-                                                                   '<tr>' +
-                                                                    '<td>' +
-                                                                        '100K API Access' +
-                                                                    '</td>' +
-                                                                  '</tr>' +
-                                                                    '<tr class="active">' +
-                                                                      '<td>' +
-                                                                        '100MB Storage' +
-                                                                      '</td>' +
-                                                                  '</tr>' +
-                                                                   '<tr>' +
-                                                                      '<td>' +
-                                                                          'Custom Cloud Services' +
-                                                                      '</td>' +
-                                                                    '</tr>' +
-                                                                  '<tr class="active">' +
-                                                                   '<td>' +
-                                                                      'Weekly Reports' +
-                                                                    '</td>' +
-                                                                    '</tr>' +
-                                                                  '</table>' +
-                                                                  '</div>' +
-                                                                  '<div class="panel-footer">' +
-                                                                    '<a  class="btn btn-success" role="button" data-toggle="modal" data-target="#payment">Sign Up</a>' +
-                                                                    '1 month FREE trial</div>' +
-                                                                    '</div>' +
-                                                                    '</div>' +'');
-                    });
-                },
-                error: function(response) {
-                  console.log('no plans available');
-                }
-
-            });
+    Components.packages();
 }
 
 
@@ -980,4 +984,3 @@ var token = $.cookie("XSRF-TOKEN");
             });
 
 }));
-
